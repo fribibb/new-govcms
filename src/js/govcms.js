5c98a8c9ed2f655f203b334e15e83b7bb584115f
Tim <tim@timhurley.net>
2015-11-18 11:30:45 +1100

    Sign up form JS fix + numerous a11y fixes + content improvements

--- src/js/govcms.js
+++ src/js/govcms.js
@@ -1,49 +1,14 @@
-// Closes the sidebar menu
-// $("#menu-close").click(function(e) {
-//     e.preventDefault();
-//     $("#sidebar-wrapper").toggleClass("active");
-// });
-
-// Opens the sidebar menu
-// $("#menu-toggle").click(function(e) {
-//     e.preventDefault();
-//     $("#sidebar-wrapper").toggleClass("active");
-// });
-
-// Scrolls to the selected menu item on the page
-// $(function() {
-//     $('a[href*=#]:not([href=#])').click(function() {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-//
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-//                 $('html,body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1000);
-//                 return false;
-//             }
-//         }
-//     });
-//
-//
-//     // make left and right split same height, noting the left is shorter
-//     rightHeight = $('.split .text-right').height();
-//     $('.split .text-left').height(rightHeight);
-//
-// });
-
-
-
 $(document).ready(function() {
+
+  // Conditional form logic
     // Set initial state, hide it...
     $('.drupal-version-list').toggleClass('sr-only');
-
     $('.drupal-version-checkbox').change(function() {
         // toggle on change
         $('.drupal-version-list').toggleClass('sr-only');
     });
 
+  // Add extra sites btn functionality
     $('.site-info-add').click(function() {
         // duplicate .site-info form element
         var htmlToInsert = $('.site-info')[0].outerHTML;
@@ -55,6 +20,5 @@ $(document).ready(function() {
         $('.site-info').last().after( htmlToInsert );
         return false;
     });
-
 
 });
