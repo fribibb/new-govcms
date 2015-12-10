db1c181ae882347140622891af1671fc2e01a881
Tim <tim@timhurley.net>
2015-12-10 16:29:39 +1100

    consistency fixes and calculator started
    btn-light made more consistent across site, now just primary and
    default versions. and eased up on the use of btn-primary too.
    
    JS and markup for calculator has been styled and done enough for a
    mockup - although actual calculation doesn’t get done for now.

--- src/js/govcms.js
+++ src/js/govcms.js
@@ -35,3 +35,42 @@ $(document).ready(function() {
     });
 
 });
+
+
+
+function priceCalc() {
+  // Setup vars
+  pageViews = $('#page-views').val();
+  siteCount = $('#number-of-sites').val();
+
+  // Calculate it:
+  totalAnnualCost = 199; // for now.....
+  totalSetupCost = 88;  // for now.....
+
+  // Display it
+  $('#calcForm').addClass('fade-out');
+  $('#calcResults #totalAnnualCost').text(totalAnnualCost);
+  $('#calcResults #totalSetupCost').text(totalSetupCost);
+  $('#calcResults #calcHeading').text('Your estimated total costs are');
+  $('#calcResults').removeClass('sr-only');
+  $('#calcResults').removeClass('fade-out');
+  $('#calcResults').addClass('fade-in');
+
+  // Stop form submission
+  return false;
+  e.preventDefault();
+}
+
+function priceReCalc() {
+  // Display it
+  $('#calcForm').removeClass('fade-out');
+  $('#calcResults').removeClass('fade-in');
+  $('#calcResults').addClass('fade-out');
+  $('#calcResults #calcHeading').text('Calculate your costs');
+  $('#calcForm').addClass('fade-in');
+  $('#calcResults').addClass('sr-only');
+
+  // Stop anchor link loading
+  return false;
+  e.preventDefault();
+}
