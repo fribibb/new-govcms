3b48e4e8e3342f62940c2a3269e36b2187346bbd
Tim <tim@timhurley.net>
2016-02-22 13:52:44 +1100

    starting on responsive styles

--- src/js/govcms.js
+++ src/js/govcms.js
@@ -23,14 +23,16 @@ $(document).ready(function() {
 
   // Make left and right split same height, noting the either may be shorter
     $( ".split" ).each(function() {
-      height1 = $('.section-one', this).height();
-      height2 = $('.section-two', this).height();
-      if (height1 > height2) {
-        rightHeight = $('.section-one', this).height();
-        $('.section-two', this).height(rightHeight);
-      } else {
-        rightHeight = $('.section-two', this).height();
-        $('.section-one', this).height(rightHeight);
+      if($( window ).width() > 993) {
+        height1 = $('.section-one', this).height();
+        height2 = $('.section-two', this).height();
+        if (height1 > height2) {
+          rightHeight = $('.section-one', this).height();
+          $('.section-two', this).height(rightHeight);
+        } else {
+          rightHeight = $('.section-two', this).height();
+          $('.section-one', this).height(rightHeight);
+        }
       }
     });
 
